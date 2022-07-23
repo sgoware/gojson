bee80da24bdfc3323a4a07353f3f59a81cfcdd51
StellarisW <744637972@qq.com>
2022-07-23 17:16:11 +0800

    feat: add string conversion

---
+++ internal/conv/string.go
@@ -0,0 +1,93 @@
+package conv
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+func String(any interface{}) string {
+	if any == nil {
+		return ""
+	}
+
+	switch value := any.(type) {
+	case string:
+		return value
+	case int:
+		return strconv.Itoa(value)
+	case int8:
+		return strconv.Itoa(int(value))
+	case int16:
+		return strconv.Itoa(int(value))
+	case int32:
+		return strconv.Itoa(int(value))
+	case int64:
+		return strconv.FormatInt(value, 10)
+	case uint:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(value), 10)
+	case uint64:
+		return strconv.FormatUint(value, 10)
+	case float32:
+		return strconv.FormatFloat(float64(value), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(value, 'f', -1, 64)
+	case bool:
+		return strconv.FormatBool(value)
+
+	case []byte:
+		return string(value)
+
+	case time.Time:
+		if value.IsZero() {
+			return ""
+		}
+		return value.String()
+	case *time.Time:
+		if value == nil {
+			return ""
+		}
+		return value.String()
+
+	default:
+		if value == nil {
+			return ""
+		}
+		var (
+			rv   = reflect.ValueOf(value)
+			kind = rv.Kind()
+		)
+		switch kind {
+		case reflect.Chan,
+			reflect.Map,
+			reflect.Slice,
+			reflect.Func,
+			reflect.Ptr,
+			reflect.Interface,
+			reflect.UnsafePointer:
+			if rv.IsNil() {
+				return ""
+			}
+		case reflect.String:
+			return rv.String()
+		}
+		// 寻找指针所指的值递归查找
+		if kind == reflect.Ptr {
+			return String(rv.Elem().Interface())
+		}
+
+		if jsonContent, err := json.Marshal(value); err != nil {
+			return fmt.Sprint(value)
+		} else {
+			return string(jsonContent)
+		}
+	}
+}
