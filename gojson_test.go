53d0114197b2066f3fa9a62bcd44dbb7f9ae034a
StellarisW <744637972@qq.com>
2022-07-24 14:58:18 +0800

    test: update

--- gojson_test.go
+++ gojson_test.go
@@ -1,66 +1,229 @@
 package gojson
 
 import (
-	"fmt"
+	"gojson/internal/mutex"
+	"reflect"
 	"testing"
 )
 
-func TestContent(t *testing.T) {
-	type Node struct {
-		Node      *Node       `json:"node"`
-		Name      string      `json:"name"`
-		Address   string      `json:"address"`
-		Age       int         `json:"age"`
-		Iface     interface{} `json:"iface"`
-		anonymous bool
-	}
+func TestNew(t *testing.T) {
 	tests := []struct {
 		name string
-		data interface{}
 		want *Json
 	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJson_LoadContent(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	type args struct {
+		data interface{}
+	}
+
+	type NODE struct {
+		Node  *NODE   `json:"node"`
+		Int   int     `json:"int"`
+		Float float64 `json:"float"`
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Json
+	}{
 		{
-			name: "json",
-			data: `{"name":"john", "score":"100"}`,
-			want: nil,
-		},
-		{
-			name: "struct",
-			data: Node{
-				Node: &Node{
-					Node: &Node{
-						Node:      nil,
-						Name:      "asd",
-						Address:   "qwe",
-						Age:       1,
-						Iface:     35,
-						anonymous: false,
-					},
-					Name:      "json",
-					Address:   "beijing",
-					Age:       18,
-					Iface:     234,
-					anonymous: false,
-				},
-				Name:      "app",
-				Address:   "shanghai",
-				Age:       10,
-				Iface:     34,
-				anonymous: false,
+			name: "array",
+			fields: fields{
+				mu:          nil,
+				jsonContent: nil,
+				isValid:     false,
 			},
+			args: args{data: []interface{}{NODE{
+				Node: &NODE{
+					Node:  &NODE{},
+					Int:   3,
+					Float: 4.33,
+				},
+				Int:   2,
+				Float: 4.544,
+			}, NODE{
+				Node: &NODE{
+					Node:  &NODE{},
+					Int:   3,
+					Float: 4.33,
+				},
+				Int:   2,
+				Float: 4.544,
+			}}},
 			want: nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			j := New().LoadContent(tt.data)
-			fmt.Println(*j.jsonContent)
-			jj := Node{}
-			j.Unmarshal(&jj)
-			fmt.Println(jj)
-			//if got := New(); !reflect.DeepEqual(got, tt.want) {
-			//	t.Errorf("New() = %v, want %v", got, tt.want)
-			//}
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if got := j.LoadContent(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Json.LoadContent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJson_LoadContentWithOptions(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	type args struct {
+		data    interface{}
+		options Options
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Json
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if got := j.LoadContentWithOptions(tt.args.data, tt.args.options); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Json.LoadContentWithOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJson_LoadFileWithOptions(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *Json
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if got := j.LoadFileWithOptions(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Json.LoadFileWithOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJson_LoadHttpResponseBodyWithOptions(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *Json
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if got := j.LoadHttpResponseBodyWithOptions(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Json.LoadHttpResponseBodyWithOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJson_Unmarshal(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	type args struct {
+		dest interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if err := j.Unmarshal(tt.args.dest); (err != nil) != tt.wantErr {
+				t.Errorf("Json.Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestJson_Get(t *testing.T) {
+	type fields struct {
+		mu          *mutex.RWMutex
+		jsonContent *interface{}
+		isValid     bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *Json
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := &Json{
+				mu:          tt.fields.mu,
+				jsonContent: tt.fields.jsonContent,
+				isValid:     tt.fields.isValid,
+			}
+			if got := j.Get(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Json.Get() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
