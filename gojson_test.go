9cd8e6e50b1965325e90f4c12021bfbb03cce6b6
StellarisW <744637972@qq.com>
2022-07-24 16:36:52 +0800

    test: update

--- gojson_test.go
+++ gojson_test.go
@@ -1,26 +1,11 @@
 package gojson
 
 import (
+	"fmt"
 	"gojson/internal/mutex"
 	"reflect"
 	"testing"
 )
-
-func TestNew(t *testing.T) {
-	tests := []struct {
-		name string
-		want *Json
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestJson_LoadContent(t *testing.T) {
 	type fields struct {
@@ -84,123 +69,6 @@ func TestJson_LoadContent(t *testing.T) {
 	}
 }
 
-func TestJson_LoadContentWithOptions(t *testing.T) {
-	type fields struct {
-		mu          *mutex.RWMutex
-		jsonContent *interface{}
-		isValid     bool
-	}
-	type args struct {
-		data    interface{}
-		options Options
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   *Json
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			j := &Json{
-				mu:          tt.fields.mu,
-				jsonContent: tt.fields.jsonContent,
-				isValid:     tt.fields.isValid,
-			}
-			if got := j.LoadContentWithOptions(tt.args.data, tt.args.options); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Json.LoadContentWithOptions() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestJson_LoadHttpResponseBodyWithOptions(t *testing.T) {
-	type fields struct {
-		mu          *mutex.RWMutex
-		jsonContent *interface{}
-		isValid     bool
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   *Json
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			j := &Json{
-				mu:          tt.fields.mu,
-				jsonContent: tt.fields.jsonContent,
-				isValid:     tt.fields.isValid,
-			}
-			if got := j.LoadHttpResponseBodyWithOptions(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Json.LoadHttpResponseBodyWithOptions() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestJson_Unmarshal(t *testing.T) {
-	type fields struct {
-		mu          *mutex.RWMutex
-		jsonContent *interface{}
-		isValid     bool
-	}
-	type args struct {
-		dest interface{}
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			j := &Json{
-				mu:          tt.fields.mu,
-				jsonContent: tt.fields.jsonContent,
-				isValid:     tt.fields.isValid,
-			}
-			if err := j.Unmarshal(tt.args.dest); (err != nil) != tt.wantErr {
-				t.Errorf("Json.Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestJson_Get(t *testing.T) {
-	type fields struct {
-		mu          *mutex.RWMutex
-		jsonContent *interface{}
-		isValid     bool
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   *Json
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			j := &Json{
-				mu:          tt.fields.mu,
-				jsonContent: tt.fields.jsonContent,
-				isValid:     tt.fields.isValid,
-			}
-			if got := j.Get(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Json.Get() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestJson_LoadFile(t *testing.T) {
 	type fields struct {
 		mu          *mutex.RWMutex
@@ -237,4 +105,19 @@ func TestJson_LoadFile(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestJson_Get(t *testing.T) {
+	node := NestedStruct{
+		Nested: &CommonStruct{
+			ExampleInt:     34,
+			ExampleFloat64: 213.3,
+			ExampleString:  "asdwq",
+		},
+		ExampleInt:     345,
+		ExampleFloat64: 2342.234,
+		ExampleString:  "asdfawf",
+	}
+	j := New().LoadContent(node)
+	fmt.Println(j.Get("example_int"))
 }
